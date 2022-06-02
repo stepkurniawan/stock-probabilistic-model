a09e531a12a3dbbc11da0b1610934ff6dbe027b0
Stephen Kurniawan <stepkurniawan@gmail.com>
2022-06-02 13:33:18 +0200

    simulation start

--- simu_stock_model.py
+++ simu_stock_model.py
@@ -5,4 +5,13 @@ Created on Thu Jun  2 12:56:02 2022
 @author: steph
 """
 
-test
+import numpy as np
+import pandas as pd
+
+# drift = normal distribution(mean, sigma)
+
+
+
+# volatility
+
+# transition matrix
