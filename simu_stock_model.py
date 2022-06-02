7d40235d2c70a946f2e1c8b3d3b036a9a90e4e60
Stephen Kurniawan <stepkurniawan@gmail.com>
2022-06-02 13:57:53 +0200

    add part drift

--- simu_stock_model.py
+++ simu_stock_model.py
@@ -14,8 +14,24 @@ import numpy as np
 import pandas as pd
 
 # drift = normal distribution(mean, sigma)
+# dim(drift) = [stock] [state] 
+# list of mean of stocks : [stock1, stock2]
+mean_stocks = [0,0]
+# list of sigma of stocks : [stock1, stock2]
+sigma_stock = [0,0]
+
+num_stocks = 2
+num_state = 3
+
+# create zero matrix
+b = np.zeros((num_stocks, num_state))
+
+for i in range(num_stocks):
+    for j in range(num_state):
+        b[i][j] = np.random.normal(mean_stocks[i], sigma_stock[j])
 
 
+print(b)
 
 # volatility
 
