f55f09337e558a955553728357500750aa0349ae
Stephen Kurniawan <stepkurniawan@gmail.com>
2022-06-04 11:18:14 +0200

    add drift complete

--- simu_stock_model.py
+++ simu_stock_model.py
@@ -20,19 +20,41 @@ mean_stocks = [0,0]
 # list of sigma of stocks : [stock1, stock2]
 sigma_stock = [0,0]
 
+# randomly generated 2 stocks
 num_stocks = 2
+
+# we have 3 states: 
+# state 0: crisis, state 1: stable, state 2: bubble
 num_state = 3
 
-# create zero matrix
-b = np.zeros((num_stocks, num_state))
+#fix the random seed
+np.random.seed(0)
 
+# b : drift for each stocks, for each state
+# ex: b[0,1] is drift for stock 0 and state 1
+b = np.random.randn(num_stocks, num_state)
+
+# b0 < b1 < b2
 for i in range(num_stocks):
     for j in range(num_state):
-        b[i][j] = np.random.normal(mean_stocks[i], sigma_stock[j])
-
+        if i == 0 and j == 0:
+            b[i,j] = np.random.randn(1)
+        else: 
+            # make sure b of the next state is larger than the one before
+            # b0 (crisis period) < b1 (stable) < b2 (bubble periode)
+            b[i,j] = np.random.normal(b[i,j-1] +1, 0.5,1) 
 
 print(b)
 
 # volatility
 
-# transition matrix
+# transition matrixs 
+# P_1 : transition matrix for stock 1
+# P_2 : transition matrix for stock 2
+P_1 = [[0.3, 0.4, 0.3],
+       [0.1, 0.9, 0.0],
+       [0.4, 0.3, 0.3]]
+
+P_2 = [[0.2, 0.6, 0.2],
+       [0.01, 0.9, 0.09],
+       [0.4, 0.3, 0.3]]
